ece034f752366fcf097e4e93784533d6eb12aaed
Dinesh Vasudevan <dvasudevan@babbel.com>
2014-12-09 17:40:11 +0100

    Yo Mama Joke

---
+++ lib/babot/plugins/yo_mama.rb
@@ -0,0 +1,44 @@
+require 'json'
+
+class YoMama
+  BASE_URL = 'http://api.yomomma.info/'
+
+  LOADABLE_EMITICONS = [
+    '(joffrey)',
+    '(joffrey)',
+    '(badjokeeel)'
+  ]
+
+  def initialize(json)
+    @json = JSON.parse(json) rescue nil
+  end
+
+  def joke
+    emoticon = LOADABLE_EMITICONS.sample(1).first
+    if @json
+      "#{emoticon} #{@json['joke']}"
+    else
+      "#{emoticon} Yo Mama is good. So no joke for you!"
+    end
+  end
+end
+
+module Babot
+  module Plugins
+    class YoMama
+      include Hipbot::Plugin
+
+      desc 'yo mama jokes'
+      on(/^yomama$/) do
+        get(::YoMama::BASE_URL) do |response|
+          if response.code == 200
+            message.mentions.each do |mention|
+              reply "#{mention} #{::YoMama.new(response.body).joke}"
+          else
+            reply("(ohgodwhy) Yo Mama is good. So no joke for you!")
+          end
+        end
+      end
+    end
+  end
+end
